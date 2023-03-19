e201e3bdf2607ed10de082e169a987d39fb6ff69
ErwinWijnveld <erwin.wijnveld@gmail.com>
2023-03-19 09:18:45 +0100

    try2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'b510362f9474c8ec6561fdb9941be402bfefcf39',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -518,12 +518,12 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'b510362f9474c8ec6561fdb9941be402bfefcf39',
             'dev_requirement' => false,
         ),
         'laravel/pint' => array(
